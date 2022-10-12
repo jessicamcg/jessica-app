5a17bae2615dc79de1cded7395f88b6a51584296
Jessica Guico <89049391+jessicamcg@users.noreply.github.com>
2022-10-12 10:03:19 -0500

    assignment prompts

---
+++ src/main/java/com/jessica/Student.java
@@ -0,0 +1,10 @@
+// ii) Create a class Student which has following methods
+//      i) Average: which would accept marks of 3 examinations & return whether the student has passed or failed depending on whether he
+//     has scored an average above 50 or not?
+//      ii) Input : which would accept the name of the student & returns the name?
+
+package com.jessica;
+
+public class Student {
+  
+}
---
+++ src/main/java/com/jessica/WashingMachine.java
@@ -0,0 +1,10 @@
+// i)  Create a washing machine class with methods as swtichO, acceptClothes, acceptDetergent, switchOFF.
+//     acceptClothes accepts the noofClothes as argument & returns the no of Clothes.
+
+
+
+package com.jessica;
+
+public class WashingMachine {
+
+}
