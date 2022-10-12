7a9e132cb7ad7136bbdd48a7fd15b61cd7cba080
Jessica Guico <89049391+jessicamcg@users.noreply.github.com>
2022-10-12 10:26:54 -0500

    vars, constructor, getters, setters

--- src/main/java/com/jessica/WashingMachine.java
+++ src/main/java/com/jessica/WashingMachine.java
@@ -6,5 +6,37 @@
 package com.jessica;
 
 public class WashingMachine {
+  private boolean power;
+  private int noofClothes;
+  private String detergent;
 
+  public WashingMachine(boolean p, int c, String d) {
+    this.setPower(p);
+    this.setNoofClothes(c);
+    this.setDetergent(d);
+  }
+
+  public boolean isPower() {
+    return power;
+  }
+
+  public void setPower(boolean power) {
+    this.power = power;
+  }
+
+  public int getNoofClothes() {
+    return noofClothes;
+  }
+
+  public void setNoofClothes(int noofClothes) {
+    this.noofClothes = noofClothes;
+  }
+
+  public String getDetergent() {
+    return detergent;
+  }
+
+  public void setDetergent(String detergent) {
+    this.detergent = detergent;
+  }
 }
